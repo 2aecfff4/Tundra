b1d54af59ecbabdc5579b7148ce481de790e45b7
2aecfff4 <11327470+2aecfff4@users.noreply.github.com>
2022-10-19 21:08:37 +0200

    vulkan_rhi: `constexpr` constants should be `inline`.

--- src/vulkan_rhi/src/vulkan_config.h
+++ src/vulkan_rhi/src/vulkan_config.h
@@ -3,13 +3,13 @@
 
 namespace tundra::vulkan_rhi {
 
-static constexpr u32 MAX_DESCRIPTOR_COUNT = (1 << 16) - 1;
+inline constexpr u32 MAX_DESCRIPTOR_COUNT = (1u << 16u) - 1u;
 
 // Bindless stuff
-static constexpr u32 NUM_BINDINGS = 4;
-static constexpr u32 BINDING_BUFFERS = 0;
-static constexpr u32 BINDING_TEXTURES = 1;
-static constexpr u32 BINDING_RW_TEXTURES = 2;
-static constexpr u32 BINDING_SAMPLERS = 3;
+inline constexpr u32 NUM_BINDINGS = 4;
+inline constexpr u32 BINDING_BUFFERS = 0;
+inline constexpr u32 BINDING_TEXTURES = 1;
+inline constexpr u32 BINDING_RW_TEXTURES = 2;
+inline constexpr u32 BINDING_SAMPLERS = 3;
 
 } // namespace tundra::vulkan_rhi
